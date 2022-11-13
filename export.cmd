079c4260a4185819a7008210cf2197b5156cfa20
shevtsov_ajax <shevtsov@ajax.systems>
2022-11-13 23:20:30 +0200

    openocd no work

---
+++ export.cmd
@@ -0,0 +1,6 @@
+set LIBCLANG_PATH=D:\espressif\Espressif/tools/xtensa-esp32-elf-clang/esp-15.0.0-20221014-x86_64-pc-windows-msvc/esp-clang/bin/libclang.dll
+set PATH=%PATH%;D:\espressif\Espressif/tools/xtensa-esp32-elf-clang/esp-15.0.0-20221014-x86_64-pc-windows-msvc/esp-clang/bin
+set PATH=%PATH%;D:\espressif\Espressif/tools/xtensa-esp32-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32-elf/bin
+set PATH=%PATH%;D:\espressif\Espressif/tools/xtensa-esp32s3-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32s3-elf/bin
+set PATH=%PATH%;D:\espressif\Espressif/tools/riscv32-esp-elf/esp-2021r2-patch5-8_4_0/riscv32-esp-elf/bin
+set PATH=%PATH%;D:\espressif\Espressif/tools/xtensa-esp32s2-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32s2-elf/bin
